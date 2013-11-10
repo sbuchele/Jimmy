c812049348f8ba65051eaf637ed8e260820241e3
SpaceTW <spacetw@att.net>
2013-11-10 07:22:53 -0600

    Hash table in main

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,8 @@
+import java.util.Date;
+import augments.Confetti;
+import augments.Friend;
+import augments.Slasher;
+
 
 public class Main {
 	
@@ -5,5 +10,49 @@ public class Main {
 		TheMonocle monocle = new TheMonocle();
 		Thread thread = new Thread(monocle);
 		thread.start();
+	}
+	
+	
+	public static Slasher<Confetti> slashTable=new Slasher<Confetti>(100000);
+	
+	public static boolean addConfetti(int number){
+		Confetti con=new Confetti(number);
+		return slashTable.add(con);
+	}
+	
+	public static double updateLoc(int number, double lat, double lon){
+		double power=0;
+		Confetti con=slashTable.sling(number);
+		if(con==null)
+			return 0;//prevent null pointer
+		con.setLat(lat);
+		con.setLon(lon);
+		for(Friend foe:con.getFriends()){
+			Confetti foeCon=slashTable.sling(foe.key());
+			if(foeCon!=null){
+				power+=foe.dist(lat, lon, foeCon.getLat(), foeCon.getLon());
+			}
+		}
+		return power;
+	}
+	
+	public static double[] getGraphLoc(int conNum, int friNum){
+		Confetti con=slashTable.sling(conNum);
+		for(Friend foe:con.getFriends()){
+			if(foe.key()==friNum){
+				return foe.getDistance();
+			}
+		}
+		return null;
+	}
+	
+	public static Date[] getGraphTime(int conNum, int friNum){
+		Confetti con=slashTable.sling(conNum);
+		for(Friend foe:con.getFriends()){
+			if(foe.key()==friNum){
+				return foe.getTime();
+			}
+		}
+		return null;
 	}
 }
--- src/augments/Slasher.java
+++ src/augments/Slasher.java
@@ -1,11 +1,10 @@
 package augments;
 
-import java.util.Date;
 
 public class Slasher<T extends Keyable> {
 	
 	
-	public static Slasher<Confetti> slashTable=new Slasher<Confetti>(100000);
+	
 	private Keyable[] table;
 	private boolean[] valid;
 			
@@ -52,45 +51,6 @@ public class Slasher<T extends Keyable> {
 	
 	
 	
-	public static boolean addConfetti(int number){
-		Confetti con=new Confetti(number);
-		return slashTable.add(con);
-	}
 	
-	public static double updateLoc(int number, double lat, double lon){
-		double power=0;
-		Confetti con=slashTable.sling(number);
-		if(con==null)
-			return 0;//prevent null pointer
-		con.setLat(lat);
-		con.setLon(lon);
-		for(Friend foe:con.getFriends()){
-			Confetti foeCon=slashTable.sling(foe.key());
-			if(foeCon!=null){
-				power+=foe.dist(lat, lon, foeCon.getLat(), foeCon.getLon());
-			}
-		}
-		return power;
-	}
-	
-	public static double[] getGraphLoc(int conNum, int friNum){
-		Confetti con=slashTable.sling(conNum);
-		for(Friend foe:con.getFriends()){
-			if(foe.key()==friNum){
-				return foe.getDistance();
-			}
-		}
-		return null;
-	}
-	
-	public static Date[] getGraphTime(int conNum, int friNum){
-		Confetti con=slashTable.sling(conNum);
-		for(Friend foe:con.getFriends()){
-			if(foe.key()==friNum){
-				return foe.getTime();
-			}
-		}
-		return null;
-	}
 	
 }
